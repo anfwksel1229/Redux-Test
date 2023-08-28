ff7cdbe8dec515a0bc04a0e54a7dae52f6d5d9f3
이용현 <anfwksel1229@gmail.com>
2023-08-28 16:49:52 +0900

    redux-renewal

---
+++ src/redux/modules/reducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+import filter from "./filter";
+import todos from "./todos";
+import users from "./users";
+import { connectRouter } from "connected-react-router";
+import history from "../../history";
+
+const reducer = combineReducers({
+    todos: todos,
+    filter: filter,
+    users: users,
+    router: connectRouter(history)
+  })
+
+  export default reducer;
